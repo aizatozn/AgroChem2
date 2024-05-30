456b3c828e78d75447d4c8c6db4d52ece7b1a922
Aizat <aizatozn@gmail.com>
2024-05-30 19:36:09 +0600

    images, images

---
+++ AgroChem/Scenes/Client/DirectoryDetails/Components/Cells/DirectoryDetailsCell.swift
@@ -0,0 +1,8 @@
+//
+//  DirectoryDetailsCell.swift
+//  AgroChem
+//
+//  Created by Aizat Ozbekova on 26/5/24.
+//
+
+import Foundation
---
+++ AgroChem/Scenes/Client/DirectoryDetails/Components/DirectoryDetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DirectoryDetailsViewModel.swift
+//  AgroChem
+//
+//  Created by Aizat Ozbekova on 26/5/24.
+//
+
+import Foundation
