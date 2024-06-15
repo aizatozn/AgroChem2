2d0dd1926ca0b3aaf85a5601cc928e1a1aecda1e
Aizat <aizatozn@gmail.com>
2024-06-15 13:31:46 +0600

    sdgf

--- AgroChem/Scenes/Client/TabBars/TabBarMainControllers/ClientDirectory/ClientDirectoryViewModel.swift
+++ AgroChem/Scenes/Client/TabBars/TabBarMainControllers/ClientDirectory/ClientDirectoryViewModel.swift
@@ -8,14 +8,15 @@
 import Combine
 import Foundation
 import XCoordinator
+import FirebaseStorage
 
 protocol ClientDirectoryViewModel: BaseVMProtocol {
     var router: UnownedRouter<ClientDirectoryRoute>? { get set }
     var counter: CurrentValueSubject<Int, Never> { get set }
     var nextRoute: PassthroughSubject<Void, Never> { get set }
-    var cancellables: Set<AnyCancellable> { get set }
     var pushToLesson: CurrentValueSubject<Int, Never> { get set }
     var selectedDirectory: CurrentValueSubject<String?, Never> { get set }
+    var cancellables: Set<AnyCancellable> { get set }
 }
 
 final class ClientExamsViewModelImpl: BaseVM<UnownedRouter<ClientDirectoryRoute>>, ClientDirectoryViewModel {
@@ -24,6 +25,7 @@ final class ClientExamsViewModelImpl: BaseVM<UnownedRouter<ClientDirectoryRoute>
     var pushToLesson = CurrentValueSubject<Int, Never>(0)
     var selectedDirectory = CurrentValueSubject<String?, Never>(nil)
     private var networkManager: NetworkManager
+    private var storage: Storage!
 
     // Additional properties for ActiveSubstances and Terms
     var activeSubstances: [ClientSecondDirectoryModel] = []
@@ -31,6 +33,7 @@ final class ClientExamsViewModelImpl: BaseVM<UnownedRouter<ClientDirectoryRoute>
 
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
+        self.storage = Storage.storage()
         super.init()
     }
 
@@ -82,26 +85,26 @@ final class ClientExamsViewModelImpl: BaseVM<UnownedRouter<ClientDirectoryRoute>
         guard let directoriesData = plistDictionary[key] as? [[String: Any]] else { return }
         let directories = directoriesData.compactMap { dictionary -> ClientDirectoryModel? in
             return ClientDirectoryModel(image: dictionary["image"] as? String ?? "",
-                                        name: dictionary["name"] as? String ?? "",
-                                        nameInEnglish: dictionary["nameInEnglish"] as? String ?? "",
-                                        name1: dictionary["name1"] as? String ?? "",
-                                        subName1: dictionary["subName1"] as? String ?? "",
-                                        name2: dictionary["name2"] as? String ?? "",
-                                        subName2: dictionary["subName2"] as? String ?? "",
-                                        name3: dictionary["name3"] as? String ?? "",
-                                        subName3: dictionary["subName3"] as? String ?? "",
-                                        name4: dictionary["name4"] as? String ?? "",
-                                        subName4: dictionary["subName4"] as? String ?? "",
-                                        name5: dictionary["name5"] as? String ?? "",
-                                        subName5: dictionary["subName5"] as? String ?? "",
-                                        name6: dictionary["name6"] as? String ?? "",
-                                        subName6: dictionary["subName6"] as? String ?? "",
-                                        name7: dictionary["name7"] as? String ?? "",
-                                        subName7: dictionary["subName7"] as? String ?? "",
-                                        name8: dictionary["name8"] as? String ?? "",
-                                        subName8: dictionary["subName8"] as? String ?? "",
-                                        name9: dictionary["name9"] as? String ?? "",
-                                        subName9: dictionary["subName9"] as? String ?? "")
+                                          name: dictionary["name"] as? String ?? "",
+                                          nameInEnglish: dictionary["nameInEnglish"] as? String ?? "",
+                                          name1: dictionary["name1"] as? String ?? "",
+                                          subName1: dictionary["subName1"] as? String ?? "",
+                                          name2: dictionary["name2"] as? String ?? "",
+                                          subName2: dictionary["subName2"] as? String ?? "",
+                                          name3: dictionary["name3"] as? String ?? "",
+                                          subName3: dictionary["subName3"] as? String ?? "",
+                                          name4: dictionary["name4"] as? String ?? "",
+                                          subName4: dictionary["subName4"] as? String ?? "",
+                                          name5: dictionary["name5"] as? String ?? "",
+                                          subName5: dictionary["subName5"] as? String ?? "",
+                                          name6: dictionary["name6"] as? String ?? "",
+                                          subName6: dictionary["subName6"] as? String ?? "",
+                                          name7: dictionary["name7"] as? String ?? "",
+                                          subName7: dictionary["subName7"] as? String ?? "",
+                                          name8: dictionary["name8"] as? String ?? "",
+                                          subName8: dictionary["subName8"] as? String ?? "",
+                                          name9: dictionary["name9"] as? String ?? "",
+                                          subName9: dictionary["subName9"] as? String ?? "")
         }
         self.router?.trigger(.directoryDetails(title: key, directories: directories))
     }
@@ -110,9 +113,9 @@ final class ClientExamsViewModelImpl: BaseVM<UnownedRouter<ClientDirectoryRoute>
         guard let activeSubstancesData = plistDictionary[key] as? [[String: Any]] else { return }
         self.activeSubstances = activeSubstancesData.compactMap { dictionary -> ClientSecondDirectoryModel? in
             return ClientSecondDirectoryModel(name: dictionary["name"] as? String ?? "",
-                                         nameInEnglish: dictionary["nameInEnglish"] as? String ?? "",
-                                         content: dictionary["content"] as? String ?? "",
-                                         description: dictionary["description"] as? String ?? "")
+                                              nameInEnglish: dictionary["nameInEnglish"] as? String ?? "",
+                                              content: dictionary["content"] as? String ?? "",
+                                              description: dictionary["description"] as? String ?? "")
         }
         self.router?.trigger(.activeSubstances(activeSubstances))
     }
@@ -121,8 +124,8 @@ final class ClientExamsViewModelImpl: BaseVM<UnownedRouter<ClientDirectoryRoute>
         guard let termsData = plistDictionary[key] as? [[String: Any]] else { return }
         self.terms = termsData.compactMap { dictionary -> ClientThirdDirectoryModel? in
             return ClientThirdDirectoryModel(name: dictionary["name"] as? String ?? "",
-                               nameInEnglish: dictionary["nameInEnglish"] as? String ?? "",
-                               description: dictionary["description"] as? String ?? "")
+                                              nameInEnglish: dictionary["nameInEnglish"] as? String ?? "",
+                                              description: dictionary["description"] as? String ?? "")
         }
         self.router?.trigger(.terms(terms))
     }
